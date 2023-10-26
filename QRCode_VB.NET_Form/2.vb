1f5422b53eea4f8826d7f2e9abadfa7a3c158293
rwhitehorn <rwhitehorn@MB-RWHITEHORN>
2023-10-26 13:30:42 -0500

    Add project files.

---
+++ QRCode_VB.NET_Form/2.vb
@@ -0,0 +1,16 @@
+﻿Imports QRCoder
+
+Public Class Form1
+    Private Sub Text_Input_TextChanged(sender As Object, e As EventArgs) Handles Text_Input.TextChanged
+
+
+
+        Dim gen As New QRCodeGenerator
+        Dim data = gen.CreateQrCode(Text_Input.Text, QRCodeGenerator.ECCLevel.Q)
+        Dim code As New QRCode(data)
+        QRCode.Image = code.GetGraphic(6)
+
+
+
+    End Sub
+End Class
---
+++ QRCode_VB.NET_Form/Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        QRCode = New PictureBox()
+        Text_Input = New TextBox()
+        CType(QRCode, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' QRCode
+        ' 
+        QRCode.AccessibleName = ""
+        QRCode.BackColor = Color.White
+        QRCode.Location = New Point(256, 25)
+        QRCode.Name = "QRCode"
+        QRCode.Size = New Size(252, 213)
+        QRCode.SizeMode = PictureBoxSizeMode.Zoom
+        QRCode.TabIndex = 0
+        QRCode.TabStop = False
+        ' 
+        ' Text_Input
+        ' 
+        Text_Input.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Text_Input.Location = New Point(231, 263)
+        Text_Input.Multiline = True
+        Text_Input.Name = "Text_Input"
+        Text_Input.Size = New Size(304, 95)
+        Text_Input.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 19F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Text_Input)
+        Controls.Add(QRCode)
+        Name = "Form1"
+        Text = "Form1"
+        CType(QRCode, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents QRCode As PictureBox
+    Friend WithEvents Text_Input As TextBox
+End Class
